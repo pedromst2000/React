0856fbd1bad0325c34db0ac4c54112f4c1f79b06
Pedro Teixeira <40210465@esmad.ipp.pt>
2023-07-24 19:30:16 +0100

    updated

--- PRACTICE/Components and Props/tutorial-project/src/App.jsx
+++ PRACTICE/Components and Props/tutorial-project/src/App.jsx
@@ -3,13 +3,14 @@ import Profile from "./Components/profile";
 
 export default function App() {
   const profiles = [
-    { id: 1, name: "Pedro Teixeira", profession: "Front Developer", age: 25 },
-    { id: 2, name: "Rita Silva", profession: "UX designer", age: 22 },
+    { id: 1, name: "Pedro Teixeira", profession: "Front Developer", age: 25, isRed: true },
+    { id: 2, name: "Rita Silva", profession: "UX designer", age: 22 , isYellow: true },
     {
       id: 3,
       name: "Ricardo Teixeira",
       profession: "Full Stack Developer",
       age: 29,
+      isGreen: true,
     },
   ];
 
@@ -18,6 +19,7 @@ export default function App() {
     { id: 2, brand: "Mercedes" },
     { id: 3, brand: "Audi" },
   ];
+
 
   return (
     <div className="container-app">
@@ -30,7 +32,10 @@ export default function App() {
             name={profile.name}
             profession={profile.profession}
             age={profile.age}
-          />
+            backgroundColor={profile.isRed ? "red" : profile.isYellow ? "yellow" : profile.isGreen ? "green" : "white"}
+            color={"black"}
+            borderRadius={"10px"}
+            />
         ))}
       </div>
       <div className="cars-container">
--- PRACTICE/Components and Props/tutorial-project/src/Components/Profile.jsx
+++ PRACTICE/Components and Props/tutorial-project/src/Components/Profile.jsx
@@ -1,8 +1,16 @@
 import PropTypes from "prop-types";
 
-export default function Profile({id, name, profession, age }) {
+export default function Profile({id, name, profession, age, backgroundColor, color, borderRadius }) {
+  
+  const stylesheet = {
+    backgroundColor: backgroundColor,
+    color: color,
+    borderRadius: borderRadius,
+  };
+
+  
   return (
-    <div key={id}>
+    <div style={stylesheet} key={id}>
       <h2>{name}</h2>
       <h3>{profession}</h3>
       <h4>{age}</h4>
@@ -15,4 +23,7 @@ Profile.propTypes = {
   name: PropTypes.string.isRequired,
   profession: PropTypes.string.isRequired,
   age: PropTypes.number.isRequired,
+  backgroundColor: PropTypes.string.isRequired,
+  color: PropTypes.string.isRequired,
+  borderRadius: PropTypes.string.isRequired,
 };
