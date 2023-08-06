e70c807a758cd178ae40de8d59f7b1db5ef51293
pedromst2000 <40210465@esmad.ipp.pt>
2023-08-06 17:19:47 +0100

    updated

--- Lists and Forms/tutorial_project 01/src/Components/WelcomeMessage.jsx
+++
@@ -1,9 +0,0 @@
-import PropTypes from "prop-types";
-
-export default function WelcomeMessage({ message }) {
-  return <h2>{message}</h2>;
-}
-
-WelcomeMessage.propTypes = {
-  message: PropTypes.string.isRequired,
-};
